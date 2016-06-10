77bc0274a667c4d540de0501a8738da07e9c5ddf
Shweta <Shweta@Shweta-PC>
2016-06-10 16:10:59 +0530

    changed

--- paytm_data/src/main/java/com/aartek/model/Recharge.java
+++ paytm_data/src/main/java/com/aartek/model/Recharge.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name="Recharge")
 public class Recharge {
-
+///helloooo
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name="id")
